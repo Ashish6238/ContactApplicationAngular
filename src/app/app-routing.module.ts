b483108e458ea0acc222e49a9fc908aea5f19f5a
Ashish Bhosale <ashish.bhosale@capgemini.com>
2022-01-11 18:42:10 +0530

    angular app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactinfoComponent } from './Contact/contactinfo/contactinfo.component';
+import { HomepageComponent } from './Contact/homepage/homepage.component';
+import { RegisterComponent } from './Contact/register/register.component';
+import { UserloginComponent } from './Contact/userlogin/userlogin.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'Contact/register',
+  component:RegisterComponent
+  },
+  {path:'Contact/userlogin',
+  component:UserloginComponent
+  },
+  {path:'Contact/contactinfo',
+  component:ContactinfoComponent
+  },
+  {path:'Contact/homepage',
+  component:HomepageComponent
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RegisterComponent } from './Contact/register/register.component';
+import { UserloginComponent } from './Contact/userlogin/userlogin.component';
+import { ContactinfoComponent } from './Contact/contactinfo/contactinfo.component';
+import { HomepageComponent } from './Contact/homepage/homepage.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterComponent,
+    UserloginComponent,
+    ContactinfoComponent,
+    HomepageComponent
   ],
   imports: [
     BrowserModule,
