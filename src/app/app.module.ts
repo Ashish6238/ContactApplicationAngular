0f75b38ee63a9670700a907d389195ca67459cf8
Ashish Bhosale <ashish.bhosale@capgemini.com>
2022-01-13 13:41:14 +0530

    thursday edit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+import {HttpClientModule} from '@angular/common/http';
+
 import { AppComponent } from './app.component';
 import { RegisterComponent } from './Contact/register/register.component';
 import { UserloginComponent } from './Contact/userlogin/userlogin.component';
 import { ContactinfoComponent } from './Contact/contactinfo/contactinfo.component';
 import { HomepageComponent } from './Contact/homepage/homepage.component';
+import { AboutComponent } from './Contact/about/about.component';
 
 @NgModule({
   declarations: [
@@ -14,11 +16,13 @@ import { HomepageComponent } from './Contact/homepage/homepage.component';
     RegisterComponent,
     UserloginComponent,
     ContactinfoComponent,
-    HomepageComponent
+    HomepageComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
