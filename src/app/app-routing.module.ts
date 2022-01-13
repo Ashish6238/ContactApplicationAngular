b86bbaef246fcd4d713790939a7025c6cd91fd70
Ashish Bhosale <ashish.bhosale@capgemini.com>
2022-01-13 13:39:30 +0530

    thursday edit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './Contact/about/about.component';
 import { ContactinfoComponent } from './Contact/contactinfo/contactinfo.component';
 import { HomepageComponent } from './Contact/homepage/homepage.component';
 import { RegisterComponent } from './Contact/register/register.component';
@@ -17,6 +18,9 @@ const routes: Routes = [
   },
   {path:'Contact/homepage',
   component:HomepageComponent
+  },
+  {path:'Contact/about',
+  component:AboutComponent
   }
 
 ];
